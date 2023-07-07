049ed6ab727b6aabe03a71fb21bf071bd99756fb
MatthewBerasa <matthew2001casla@gmail.com>
2023-07-07 16:46:39 -0400

    Lab 7

--- lab_assignment_7.c
+++ lab_assignment_7.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void swap(int* x, int* y, int* swapCount);
-int bubbleSort(int arr[], int arrSize);
+void swap(int* x, int* y, int* swapArr, int index);
+int* bubbleSort(int arr[], int arrSize);
 
 int main() {
 	int arr[] = { 97, 16, 45, 63, 13, 22, 7, 58, 72 };
@@ -11,30 +12,33 @@ int main() {
 	for (int i = 0; i < arrSize; i++)
 		printf("%d ", arr[i]);
 
-	printf("\n\nBubble Sort Numbers of Swap: %d", bubbleSort(arr, arrSize));
+	int* swapArr = bubbleSort(arr, arrSize);
+	puts("\n\nNumber of Swap Per Index: ");
+	for(int z = 0; z < arrSize; z++)
+		printf("Arr[%d]: %d\n", z, swapArr[z]);
 
 	puts("\n\nArray After Swap: ");
 	for (int j = 0; j < arrSize; j++)
 		printf("%d ", arr[j]);
 }
 
-void swap(int* x, int* y, int* swapCount) {
+void swap(int* x, int* y, int* swapArr, int index) {
 	int temp = *x;
 	*x = *y;
 	*y = temp;
-	(*swapCount)++;
+	(*(swapArr + index))++;
 }
 
-int bubbleSort(int arr[], int arrSize) {
-	int swapCount = 0;
+int* bubbleSort(int arr[], int arrSize) {
+	int* swapArr = (int*)calloc(arrSize, sizeof(int));
 
 	for (int i = 0; i < arrSize - 1; i++) {
 		for (int j = 0; j < arrSize - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
-				swap(arr + j, arr + j + 1, &swapCount);
+				swap(arr + j, arr + j + 1, swapArr, j);
 			}
 		}
 	}
 
-	return swapCount;
+	return swapArr;
 }
